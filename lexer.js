786df27d7912c0d73c707c8e20a417f60c34b60b
Anna Clayton <claytonannam@gmail.com>
2013-02-16 21:36:16 -0500

    First attempt at parse

--- lexer.js
+++ lexer.js
@@ -2,135 +2,238 @@
 /* February 2013 */
 /* lexer.js  */
 
-    const T_INT = "int_type";
-    const T_CHAR = "char_type";
-    const T_DOLLAR = "end_of_file";
-    const T_PRINT = "print";
-    const T_RPAREN = "rparen";
-    const T_LPAREN = "lparen";
-    const T_RBRACKET = "rbracket";
-    const T_LBRACKET = "lbracket";
-    const T_PLUS = "plus";
-    const T_MINUS = "minus";
-    const T_QUOTE = "quote";
-    const T_STRING = "string";
-    const T_EQUAL = "assign";
-    
-    const K_OPERAND = "operand";
-    const K_TYPE = "type";
-    const K_INT = "integer";
+    const K_INT = "int_type";
+    const K_CHARTYPE = "char_type";
+    const K_DOLLAR = "end_of_file";
+    const K_PRINT = "print";
+    const K_RPAREN = "rparen";
+    const K_LPAREN = "lparen";
+    const K_RBRACKET = "rbracket";
+    const K_LBRACKET = "lbracket";
+    const K_PLUS = "plus";
+    const K_MINUS = "minus";
+    const K_QUOTE = "quote";
     const K_STRING = "string";
+    const K_EQUAL = "assign";
+    const K_OPERAND = "operand";
+    const K_DIGIT = "digit";
+    const K_CHAR = "char";
+    const K_TYPE = "type";
     const K_ID = "identifier";
-    const K_TERMINATOR = "terminator";
+    
+    const eErrorsFound = "Errors were found";
+    const wMoreInput = "Input after EOF ignored";
+    const wNoEOF = "Missing EOF meta-symbol was inserted";
     
     // Set up hash of all reserved words and which constant they go with
-    var ReservedWords = { int: T_INT,
-                          Int: T_INT,
-                          P: T_PRINT,
-                          char: T_CHAR,
-                          $: T_DOLLAR
+    var ReservedWords = { int: {lexeme: K_TYPE, value: K_INT},
+                          P: {lexeme: K_PRINT},
+                          char: {lexeme: K_TYPE, value: K_CHARTYPE},
                           };
                           
 
-var Token = function(k, v, l, toknum){
+var Token = function(k, v, l, w, toknum){
     // Keep a count of tokens
     if (Token.counter == undefined) {
-        Token.counter = 0;
+        Token.counter = 1;
     } else {
         Token.counter++;
     }
-    var t = {kind: k, value: v, loc: l, toknum: Token.counter};
-
+    var t = {kind: k, value: v, loc: l, warn: w, toknum: Token.counter};
+    
+    t.lineOutput = function() {
+	if (t.loc) 
+	    return " at line # " + t.loc;
+	else
+	    return ""
+    }
+    
+    t.warnOutput = function() {
+	if (t.warn)
+	    return "\n**Warning: " + t.warn + "**";
+	else
+	    return ""
+	
+    }
     t.toString = function(){
-        return t.toknum + ": <" + t.kind + " : "+t.value+"> at line # "+t.loc + "\n";
+	    return t.toknum + ": <" + t.kind + " : "+t.value+"> " + t.lineOutput() + t.warnOutput();
     }
     return t;
 }
 
-    function lex()
+    function lex(idList)
     {
+	Token.counter = null;
         var tokens = {};
         tokens = new Array();
 	var returnString = "";
+	var errors = {};
+	errors = new Array();
+	var inQuotes = false;
+	var quotesBegin = 0;
+	var foundEOF = false;
+	
+	errors.push(eErrorsFound);
+	
 	// Grab the "raw" source code.
-//	var sourceCode = document.getElementById("taSourceCode").value;
-//	var sourceCode = $('#textAreaWithLines').getElementById("taSourceCode").value;
-        
-
-        var sourceCode = document.getElementById("taSourceCode").value;
+        var sourceCode = document.getElementById("taSourceCode").value.trim();
       //  alert(sourceCode);
 	
 	// Break up the source code by line 
 	lines = sourceCode.split(/\n/);
 	
 	// Process each line:
-	for(var line=0; line<lines.length; line++) {
+	for(var line=0; line<lines.length && foundEOF == false; line++) {
 	    tempString = '';
 	    // Add spaces around all the punctuation so I can tokenize the string
 	    for(var i=0; i<lines[line].length; i++){
-	      if (/[\$\(\)\+\-\{\}\"]/.test(lines[line].charAt(i)))
-	        tempString += ' ' + lines[line].charAt(i) + ' ';
-	      else
-	        tempString += lines[line].charAt(i);
+		if (/[\$\(\)\+\-\=\{\}\"]/.test(lines[line].charAt(i)))
+		    tempString += ' ' + lines[line].charAt(i) + ' ';
+		else
+		    tempString += lines[line].charAt(i);
 	    }
      
 	   // Tokenize the string. Return a list of anything surrounded by spaces
 	    words = tempString.match(/\S+/g);
+	    
+	    // If we found any tokens on this line, 
             if (words) {
-            for (var j = 0; j < words.length; j++) {
-            //  alert(words[j]);
-            switch(true) {
-            case (/\d/.test(words[j])): 
-                tokens.push(Token(K_INT, words[j], line+1));
-                break;
-            
-            case (words[j] in ReservedWords):
-                tokens.push(Token(ReservedWords[eval("\"" + words[j] + "\"")], null, line+1))
-                break;
-            
-            case (words[j] == "+"):
-                tokens.push(Token(K_OPERAND, T_PLUS, line+1))
-                break;
-                
-            case (words[j] == "-"):
-                tokens.push(Token(K_OPERAND, T_MINUS, line+1))
-                break;
-                
-            case (words[j] == "="):
-                tokens.push(Token(K_OPERAND, T_EQUAL, line+1))
-                break;
-            
-            case (words[j] == "("):
-                tokens.push(Token(K_TERMINATOR, T_RPAREN, line+1))
-                break;
-            
-            case (words[j] == ")"):
-                tokens.push(Token(K_TERMINATOR, T_LPAREN, line+1))
-                break;
-            
-            case (words[j] == "{"):
-                tokens.push(Token(K_TERMINATOR, T_RBRACKET, line+1))
-                break;
-            
-            case (words[j] == "}"):
-                tokens.push(Token(K_TERMINATOR, T_LBRACKET, line+1))
-                break;
-            
-            case (words[j] == "\""):
-                tokens.push(Token(T_QUOTE, null, line+1))
-                break;
-            
-            case (/[a-z]/.test(words[j]) && words[j].length == 1):
-                tokens.push(Token(K_ID, words[j], line+1))
-                break;
-            
-            default:
-                alert("I don't know what to do with " + words[j]);
-                break;
-            }
-            }
-            }
-        }   
-	return tokens;      
-    }
+		//alert(words)
+		// For each of them
+		for (var j = 0; j < words.length && foundEOF == false; j++) {
+		//  alert(words[j]);
+		    // Figure out what type of lexeme it is
+		    switch(true) {
+			
+		    	// If the next token is a double quote
+			case (words[j] == "\""):
+			    tokens.push(Token(K_QUOTE, null, line+1))
+			    // If we're currently "in quotes" and this is a terminating quote, turn off the inQuotes indicator.
+			    // Otherwise this is the first quotes of a pair, so turn on the inQuotes indicator.
+			    // Note that if there are mismatched quotes, inQuotes will be true at the end of lexical analysis. This is bad!
+			    if (inQuotes)
+				inQuotes = false
+			    else {
+				inQuotes = true
+				quotesBegin = line+1
+			    }
+			break;
+		    
+			// If the last token seen was a quote
+			case (inQuotes):
+			// processing a charstring within quotes
+			// deal with the case where end of file is seen before the end quote is seen
+			// break everything within quotes to character tokens
+			// Note that spaces within quotes have been stripped. Need to think of a way to fix this for round 2.
+			    chars = words[j].split('');
+			    for (var l=0; l<chars.length; l++) {
+				if (/[a-z]/.test(chars[l])) {
+				    tokens.push(Token(K_CHAR, chars[l], line+1))
+				}
+				else {
+				    errors.push(chars[l] + " not allowed as part of a character string on line " + eval(line+1));
+				}
+			    }			    
+			break;
+		    
+			// If the next token is a "digit" -- int of length = 1
+			case (/\d/.test(words[j]) && words[j].length == 1):
+			    // Numbers may only be 1 digit
+			    tokens.push(Token(K_DIGIT, words[j], line+1));
+			break;
+		    
+			// If the next token is a recognized reserved word
+			case (words[j] in ReservedWords):
+			    tokens.push(Token(ReservedWords[eval("\"" + words[j] + "\"")].lexeme, ReservedWords[eval("\"" + words[j] + "\"")].value, line+1))
+			break;
+		    
+			// If the next token is a plus sign
+			case (words[j] == "+"):
+			    tokens.push(Token(K_OPERAND, K_PLUS, line+1))
+			break;
+			
+			// If the next token is a minus sign
+			case (words[j] == "-"):
+			    tokens.push(Token(K_OPERAND, K_MINUS, line+1))
+			break;
+			
+			// If the next token is an equals sign
+			case (words[j] == "="):
+			    tokens.push(Token(K_EQUAL, null, line+1))
+			break;
+		    
+			// If the next token is a right paren
+			case (words[j] == "("):
+			    tokens.push(Token(K_LPAREN, null, line+1))
+			break;
+		    
+			// If the next token is a left paren
+			case (words[j] == ")"):
+			    tokens.push(Token(K_RPAREN, null, line+1))
+			break;
+		    
+			// If the next token is a right bracket
+			case (words[j] == "{"):
+			    tokens.push(Token(K_LBRACKET, null, line+1, null))
+			break;
+		    
+			// If the next token is a left bracket
+			case (words[j] == "}"):
+			    tokens.push(Token(K_RBRACKET, null, line+1))
+			break;
+		    
+			// If the next token is the end-of-input meta-symbol, $
+			case (words[j] == "$"):
+			    // Indicate that EOF symbol was seen
+			    foundEOF = true
+			    // If we're not at the end of the input, push the token with a warning
+			    // Otherwise just push the token
+			    if (j<words.length - 1|| line < lines.length - 1)
+				tokens.push(Token(K_DOLLAR,null,line+1,wMoreInput))
+			    else
+				tokens.push(Token(K_DOLLAR,null,line+1))
+			break;
+			
+			// If the next token is a valid identifier
+			case (/[a-z]/.test(words[j]) && words[j].length == 1):
+			// For round 1, all identifiers are 1 character in length.
+			// May need a better RE to identify a valid >1 length identifier for the final.  
+			    tokens.push(Token(K_ID, words[j], line+1))
+			// Add each identifier to the identifier list
+			    idList[words[j]] = line+1;
+			break;
+		    
+			// The next couple cases are for known errors with custom error messages
+		    	case (/\d/.test(words[j]) && words[j].length > 1):
+			    // Numbers are only 1 digit in round 1. Give a descriptive error message if int > 9
+			    errors.push("Integers must be <= 9.  Integer with value " + words[j] + " seen on line " + eval(line+1));
+			break;
+		    
+			// Default case is something completely unknown
+			default:
+			    errors.push("Unknown lexeme: " + words[j] + " on line " + eval(line+1));
+			break;
+		    } // End select
+		} // End for each word
+            } // End if words ! null
+        } // End for each line'
+	
+	// If the end of input has been reached and a begin-quote was seen but an end-quote wasn't, give an error.
+	// There are probably lots and lots of other errors already due to non-valid characters within quotes, so this
+	// error is just icing on the cake. But it's the good kind of icing, because it's probably easier to interpret/fix the error.  
+	if (inQuotes)
+	    errors.push("Unterminated quotes beginning on line " + quotesBegin);
+	    
+	// If all the input was processed and the EOF meta-symbol wasn't seen, push it with a warning
+	if (foundEOF == false)
+	    tokens.push(Token(K_DOLLAR,null,null,wNoEOF))
+	    
+	// If at least one error was found (errors[0] is a string constant kErrors), then return
+	// the array of errors. Otherwise return the array of tokens (The tokens include warning messages
+	// where appropriate).  
+	if (errors.length > 1)
+	    return errors;
+	else
+	    return tokens;      
+    } // End function lex
 
---
+++ parser.js
@@ -0,0 +1,188 @@
+/* Anna Clayton */
+/* February 2013 */
+/* parser.js  */
+
+	const kConsume = 1;
+	const kCheck = 0;
+	const kFailure = 0;
+	const kSuccess = 0;
+	
+	var tokenIndex = 0;
+	var thisToken = new Token();
+	var nextToken = new Token();
+	
+         function parse() {
+	    putMessage("taOutput","parse()");
+	    var results = kFailure;
+	    results = parseProgram();
+	    return results;
+	}
+	
+	function parseProgram() {
+	    say("ParseProgram()");
+	    parseStatement();
+	    if (eatThisToken(K_DOLLAR) == kSuccess)
+		return kSuccess
+	    else
+		return kFailure
+	}
+	
+	function parseStatement() {
+	    say("ParseStatement()");
+	    thisToken = checkToken(kConsume)
+	    switch (true) {
+		case (thisToken.kind == K_PRINT): {
+		    parsePrintStatement();
+		    break;
+		}
+		case (thisToken.kind == K_ID): {
+		    parseAssignStatement();
+		    break;
+		}
+		case (thisToken.kind == K_TYPE): {
+		    parseVarDecl();
+		    break;
+		}
+		case (thisToken.kind == K_LBRACKET): {
+		    parseStatementList();
+		    break;
+		}
+		default:
+		   return kFailure;
+	    }
+	    return kSuccess;
+	}
+	
+	function parsePrintStatement() {
+	    say("parsePrintStatement()");
+	    
+	    if (eatThisToken(K_LPAREN) == kSuccess)
+		; // Keep going
+	    else
+		return kFailure
+	    
+	    parseExpr()
+	
+	    if (eatThisToken(K_RPAREN) == kSuccess)
+		return kSuccess
+	    else
+		return kFailure
+	}
+	
+	function parseAssignStatement() {
+	    say("parseAssignStatement()");
+	    if (eatThisToken(K_EQUAL) == kSuccess)
+		; // Keep going
+	    else
+		return kFailure
+	    
+	    thisToken = checkToken(kConsume)
+	    parseExpr()
+	    
+	    return kSuccess
+	}
+	
+	function parseStatementList() {
+	    say("ParseStatementList()");
+	    if (checkToken(kCheck).kind != K_RBRACKET) {
+		parseStatement();
+		say ("Back in parseStatementList(), recursing ")
+		parseStatementList();
+	    }
+	    else {	
+	    if (eatThisToken(K_RBRACKET) == kSuccess)
+		return kSuccess;
+	    else
+		return kFailure
+	    }
+	    return kSuccess;
+	}
+	
+	function parseExpr() {
+	    say("ParseExpr()");
+	    if (checkToken(kCheck).kind == K_DIGIT)
+		parseIntExpr()
+	    else if (checkToken(kCheck).kind == K_QUOTE)
+		parseCharExpr()
+	    else if (checkToken(kCheck).kind == K_ID)
+		return kSuccess;
+	    else return kFailure;
+	    return kSuccess;
+	}
+	
+	function parseIntExpr() {
+	    say("ParseIntExpr()");
+	    if (eatThisToken(K_DIGIT) == kSuccess)
+		; // Keep going
+	    else
+		return kFailure
+	    if (checkToken(kCheck).kind != K_OPERAND)
+		return kSuccess;
+	    else {
+		eatThisToken(K_OPERAND);
+		parseIntExpr();
+	    }
+	    return kSuccess;
+	}
+	
+	function parseCharExpr() {
+	    say("ParseCharExpr()");
+	    if (eatThisToken(K_QUOTE) == kSuccess)
+		; // Keep going
+	    else
+		return kFailure
+	    parseCharList();
+	    if (eatThisToken(K_QUOTE) == kSuccess)
+		; // Keep going
+	    else
+		return kFailure
+	    return kSuccess;
+	}
+	
+	function parseCharList() {
+	    say("ParseCharList()");
+	    if (checkToken(kCheck).kind != K_QUOTE) {
+		if (eatThisToken(K_CHAR) == kSuccess)
+		    ; // Keep going
+		else
+		    return kFailure
+		parseCharList();
+	    }
+	    return kSuccess;
+	}
+	
+	function parseVarDecl() {
+	    say("ParseVarDecl()");
+	    if (eatThisToken(K_ID) == kSuccess)
+		return kSuccess
+	    else
+		return kFailure
+	}
+	
+	function eatThisToken(tokenKind) {
+	    thisToken = checkToken(kConsume);
+	    say("Checking for " + tokenKind)
+	    if (thisToken.kind == tokenKind) {
+		say("  Found " + tokenKind + "!")
+		return kSuccess;
+	    }
+	    else {
+		say("  Didnt find " + tokenKind + ". Found " + thisToken.kind + " instead.")
+		return kFailure;
+	    }
+	}
+	
+	function checkToken(consume) {
+	    var indexToGet = tokenIndex;
+	    if (consume == kConsume) {
+		say("Consumed " + tokens[tokenIndex])
+		tokenIndex = tokenIndex + 1;
+	    }
+	    else
+		say("Checked but did not consume "+tokens[tokenIndex])
+	    return tokens[indexToGet];
+	}
+	
+	function say(msg) {
+	    putMessage("taOutput",msg)
+	}
